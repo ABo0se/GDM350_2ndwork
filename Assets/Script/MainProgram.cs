b909660e0338a2a5954ff71d198a4a8502ea400c
ABoOse <thanawat.65938@mail.kmutt.ac.th>
2021-01-31 15:52:23 +0700

    FirstCommit2ndwork

---
+++ Assets/Script/MainProgram.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class MainProgram : MonoBehaviour
+{
+    List<object> Randomlist = new List<object>();
+    //public TextMeshProUGUI TextInputresult;
+    [SerializeField] TextMeshProUGUI TextOutputresult;
+    [SerializeField] Button RandomScript;
+    [SerializeField] TMP_InputField numberofinput1;
+    [SerializeField] TMP_InputField input1;
+    [SerializeField] TMP_InputField input2;
+    double mininput, maxinput, numberofinput;
+    int numberofinputb;
+    int checkerror;
+    string actualresult;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset2();
+    }
+
+    public void GetInputOnClickHandler()
+    {
+        ResetOnlyAnswer();
+        checkerror = 0;
+        numberofinput = double.Parse(numberofinput1.text);
+        mininput = double.Parse(input1.text);
+        maxinput = double.Parse(input2.text);
+
+        if (mininput % 1 == 0 && maxinput % 1 == 0 && numberofinput % 1 == 0)
+        {
+            Randomwork();
+            Result();
+        }
+        else
+        {
+            checkerror = 1;
+            Result();
+        }
+    }
+    public void Result()
+    {
+        if (checkerror == 0)
+        {
+            actualresult = String.Join(", ", Randomlist);
+        }
+        else
+        {
+            actualresult = "Error! Please Try again.";
+        }
+        TextOutputresult.text = actualresult;
+    }
+    public void Reset2()
+    {
+        Randomlist.Clear();
+        TextOutputresult.text = "-";
+        numberofinput1.text = "Enter Number...";
+        input1.text = "Enter Number...";
+        input2.text = "Enter Number...";
+    }
+    public void ResetOnlyAnswer()
+    {
+        TextOutputresult.text = "-";
+        Randomlist.Clear();
+    }
+    public void Randomwork()
+    {
+        numberofinputb = 1;
+        while (numberofinputb <= numberofinput)
+        {
+            Randomizer Random1 = new Randomizer((int)mininput, (int)maxinput);
+            Random1.Roll();
+            Randomlist.Add(Random1.Randomresult);
+            numberofinputb++;
+        }
+    }
+}
---
+++ Assets/Script/Randomize.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Randomize : MonoBehaviour
+{
+    static Random rand;
+    public static void Initialize()
+    {
+        rand = new Random();
+    }
+    public static int Next(int minvalue, int maxvalue)
+    {
+        return Random.Range(minvalue, (maxvalue + 1));
+    }
+}
+
+public class Randomizer : MonoBehaviour
+{
+    int randomresult;
+    int minval;
+    int maxval;
+    [SerializeField] TMP_InputField datainput1;
+    [SerializeField] TMP_InputField datainput2;
+
+    public Randomizer(int minval, int maxval)
+    {
+        this.minval = minval;
+        this.maxval = maxval;
+    }
+
+    public int Randomresult
+    {
+        get
+        {
+            return randomresult;
+        }
+    }
+    public void Roll()
+    {
+        randomresult = Randomize.Next(minval, maxval);
+    }
+}
